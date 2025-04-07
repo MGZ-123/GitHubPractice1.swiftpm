65fd5569769c4cc58bc4321d934d966f73f12a26
Martin Zapryanov <mzapryanov7451@stu.d214.org>
2025-04-07 13:33:36 -0500

    I added a capsule shape

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Martin")
+        Capsule(style: .circular)
+            .foregroundStyle(.cyan)
+            .frame(width: 200, height: 100)
         VStack {
             Text("Matei")
         }
