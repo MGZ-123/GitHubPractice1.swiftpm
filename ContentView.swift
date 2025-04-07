c39c61f0098c88f30b2d9fd5362ee07994669e2f
Matei Donisa <mdonisa8105@stu.d214.org>
2025-04-07 13:32:54 -0500

    I added a image that rotates

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rotationAngle = 0.0
     var body: some View {
         Text("Martin")
-        VStack {
-            Text("Matei")
-        }
+        Text("Matei")
+        Image(systemName: "volleyball")
+            .foregroundStyle(.red)
+            .rotationEffect(Angle(degrees:rotationAngle))
+            .onTapGesture {
+                withAnimation(.easeInOut.speed(0.4)){
+                    rotationAngle += 360
+                }
+            }
     }
 }
